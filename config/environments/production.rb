8e76a6b7787368a4bffef7262a73e94295791c93
LVillaflores <54790518+LVillaflores@users.noreply.github.com>
2019-09-24 08:24:07 +0800

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
-  host = frozen-mountain-46958.heroku.app
+  host = 'frozen-mountain-46958.heroku.app'
 
   config.action_mailer.default_url_options = { host: host}
   ActionMailer::Base.smtp_settings ={

b15720916217b642f481b0a0e36f17faaf558c06
LVillaflores <lorenzo@tanda.co>
2019-09-25 10:01:28 +0800

    Fixed Account Activation mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   	:authentication 				=> :plain,
   	:user_name 							=> ENV['SENDGRID_USERNAME'],
   	:password 							=> ENV['SENDGRID_PASSWORD'],
-  	:domain									=> 'herokuapp.com',
+  	:domain									=> 'heroku.app.com',
   	:enable_starttle_auto		=> true
   }
 
